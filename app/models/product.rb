ddf931d0417f24de9893916d12ad137243d5ff58
Tiffany Murphy <tiffanymurphy73@gmail.com>
2017-09-23 17:06:33 -0500

    5.2 added search form to nav and added form styles

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,10 @@
 class Product < ApplicationRecord
 	has_many :orders
-end
-
-def self.search(search_term)
-	if Rails.env.production?
-		Product.where("name ilike ?", "%#{search_term}")
-	else
-		Product.where("name LIKE ?", "%#{search_term}")
+	def self.search(search_term)
+		if Rails.env.production?
+			Product.where("name ilike ?", "%#{search_term}")
+		else
+			Product.where("name LIKE ?", "%#{search_term}")
+		end
 	end
 end
