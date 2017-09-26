132f845fb3b4b6d7b97a0fdf1352e5018fe08b56
Tiffany Murphy <tiffanymurphy73@gmail.com>
2017-09-25 21:58:36 -0500

    5.2 fixed search

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
 	has_many :orders
 
 	def self.search(search_term)
-		Product.where("name LIKE ?", "%#{search_term}%")
+		Product.where("name ILIKE ?", "%#{search_term}%")
 	end
 end
