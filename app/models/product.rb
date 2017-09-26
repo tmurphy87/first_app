50847b931c8ba46d14a413e830b52f323329ed12
Tiffany Murphy <tiffanymurphy73@gmail.com>
2017-09-25 20:07:48 -0500

    5.2 fixed search bar

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
 	has_many :orders
 
 	def self.search(search_term)
-		Product.where("lower(name) LIKE lower(?)", "%#{search_term}%")
+		like_operator = Rails.env.development? ? 'like' : 'ilike'
+		Product.where("name #{like_operator} ?", "%#{search_term}%")
 	end
 end
