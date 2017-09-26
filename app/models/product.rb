ca11e19faf0a04609bba319dd06ea38bcd721ed8
Tiffany Murphy <tiffanymurphy73@gmail.com>
2017-09-25 20:16:47 -0500

    5.2 fixed search

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 	has_many :orders
 
 	def self.search(search_term)
-		like_operator = Rails.env.development? ? 'like' : 'ilike'
+		like_operator = Rails.env.production? ? 'ilike' : 'like'
 		Product.where("name #{like_operator} ?", "%#{search_term}%")
 	end
 end
