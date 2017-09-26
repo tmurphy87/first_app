449312ddff9c88409c8e93eb8bb3773349c57b22
Tiffany Murphy <tiffanymurphy73@gmail.com>
2017-09-26 15:28:00 -0500

    4.6 added validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@ class Product < ApplicationRecord
 	has_many :orders
 
 	validates :name, :price, :image_url, :color, presence: true
-	validates :price, numericality: true
 
 	def self.search(search_term)
 		Product.where("lower(name) LIKE lower(?)", "%#{search_term}")
