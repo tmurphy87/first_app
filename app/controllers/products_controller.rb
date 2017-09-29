089ec7128abc5172759cbd9330f599a1112ad633
Tiffany Murphy <tiffanymurphy73@gmail.com>
2017-09-29 15:22:03 -0500

    5.5 fixing authentication branch with changes to authorization exercise

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,14 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
   before_filter :authenticate_user!
   respond_to :json, :html
->>>>>>> Stashed changes
-=======
-  respond_to :json, :html
->>>>>>> master
 
   # GET /products
   # GET /products.json
