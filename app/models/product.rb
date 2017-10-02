c8612d69c768bc0dd26d5e1c47e161566abe2a9e
Tiffany Murphy <tiffanymurphy73@gmail.com>
2017-10-01 20:11:25 -0500

    5.7 Comment Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,18 @@
 class Product < ApplicationRecord
 	has_many :orders
+	has_many :comments
 
 	validates :name, :price, :image_url, :color, presence: true
 
 	def self.search(search_term)
 		Product.where("name ILIKE ?", "%#{search_term}%")
+	end
+
+	def highest_rating_comment
+		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
 	end
 end
