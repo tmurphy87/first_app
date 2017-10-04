c7b6a43132bcdb18ea6ef747878295f3e60cb150
Tiffany Murphy <tiffanymurphy73@gmail.com>
2017-10-04 16:18:09 -0500

    5.10 Admin Role and Moderating

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new #guest user (not logged in)
-    can :manage, User, id: user.id   
+
+    if user.admin?
+    	can :manage, :all
+    else
+    	can :manage, User, id: user.id
+    end 
   end
 end
 
