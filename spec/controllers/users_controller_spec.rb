aa0406649adbbfb6ef54cb6705387ceea02b1d4d
Tiffany Murphy <tiffanymurphy73@gmail.com>
2017-11-06 12:16:54 -0600

    6.3 functional tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,7 +24,7 @@ describe UsersController, type: :controller do
 		context "user is not logged in" do
 			it "redirects to login" do
 				get :show, params: { id: @user.id }
-				expect(response).to redirect_to(new_user_session_path)
+				expect(response).to redirect_to(root_path)
 		  end
 		end
 
